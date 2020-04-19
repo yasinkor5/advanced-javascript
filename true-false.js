1fcedb8b4da20a357d3b76e106ee30c1d6f7ba5d
yasinkor5 <yasinkoreabd5@gmail.com>
2020-04-19 09:42:53 +0900

    true-false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+//falsy;
+//0, "" , undefined, null, NuN, yasin; 
+
+//Truthy
+//"yasin", " "; [], {}; "false";
+const name = "false"
+
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
