54a980dd6826065ab08cab2f11f6fcf3f66a51c9
yasinkor5 <yasinkoreabd5@gmail.com>
2020-04-19 10:10:03 +0900

    null-undefined

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+//let name
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1,num2){
+//     console.log(num1, + num2);
+
+// }
+
+// const result = add(3, 4);
+// console.log(result);
+
+// console.log(result)
+
+let fun = undefined;
+console.log(fun);
