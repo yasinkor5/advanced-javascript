02d9a905278f6973af1dcd12f5a93dce331362b9
yasinkor5 <yasinkoreabd5@gmail.com>
2020-04-20 12:22:35 +0900

    scope,bolck scope

---
+++ scope.js
@@ -0,0 +1,10 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(result);
+return result;
+}
+const output = sum(3,7);
+console.log(output);
+console.log(bonus);
