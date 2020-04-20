b665d785bf9876f08439c14b1bee97e3b6979f6b
yasinkor5 <yasinkoreabd5@gmail.com>
2020-04-20 12:50:15 +0900

    splice

---
+++ arraysplice.js
@@ -0,0 +1,10 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//const part = nums.slice(2, 5);
+const removed = nums.splice(2, 5, 44, 55, );
+
+//console.log(part);
+//console.log(nums);
+//console.log(removed)
+
+const together = nums.join("y ")
+console.log(together)
